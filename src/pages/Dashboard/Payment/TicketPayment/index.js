ebf7dcafcc29f016b67d2db8c971765826f9d9d0
V-Emanuel <gvictor567@gmail.com>
2023-05-18 00:34:59 -0300

    fix: payment form

--- src/pages/Dashboard/Payment/TicketPayment/index.js
+++ src/pages/Dashboard/Payment/TicketPayment/index.js
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
-import { PaymentContent, TicketContent, PaymentConfirme, ConfirmeIcon, CardContent } from './style';
+import { PaymentContent, TicketContent, PaymentConfirme, ConfirmeIcon } from './style';
 import useToken from '../../../../hooks/useToken';
-import Cards from 'react-credit-cards-2';
-import 'react-credit-cards-2/dist/es/styles-compiled.css';
 import { createPaymentParams } from '../../../../services/paymentApi';
+import PaymentTest from '../../../../components/Dashboard/Payment/PaymentForm';
 
-const PaymentForm = ({ ticket }) => {
-  const ticketPriceAjust = ticket.TicketType.price / 100;
+function PaymentForm({ ticket }) {
+  const ticketPriceAjust = 1000; //ticket.TicketType.price / 100;
   const token = useToken();
-  const [number, setNumber] = useState('');
-  const [expiry, setExpity] = useState('');
-  const [cvc, setCvc] = useState('');
-  const [name, setName] = useState('');
-  const [focus, setFocus] = useState('');
   const [issuer, setIssuer] = useState('');
   const [userTicket, setUserTicket] = useState(ticket);
 
-  const handleInputFocus = (e) => {
-    setFocus(e.target.name);
-  };
-
-  const handleCardTypeChange = (cardType) => {
-    setIssuer(cardType.issuer);
-  };
+  const [state, setState] = useState({
+    number: '',
+    expiry: '',
+    cvc: '',
+    name: '',
+    focus: '',
+  });
 
   function Paid() {
     return (
@@ -41,55 +35,7 @@ const PaymentForm = ({ ticket }) => {
   function Reserve() {
     return (
       <>
-        <CardContent>
-          <Cards
-            cvc={cvc}
-            expiry={expiry}
-            focused={focus}
-            name={name}
-            number={number}
-            callback={handleCardTypeChange}
-          />
-          <form>
-            <input
-              type="number"
-              name="number"
-              placeholder="Card Number"
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-              onFocus={handleInputFocus}
-              required
-            />
-            <h5>E.g.: 49..., 51..., 36..., 37...</h5>
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              onFocus={handleInputFocus}
-            />
-            <input
-              type="number"
-              name="expiry"
-              placeholder="Valid Thru"
-              value={expiry}
-              onChange={(e) => setExpity(e.target.value)}
-              onFocus={handleInputFocus}
-              className="validThru"
-              mask="99/99"
-            />
-            <input
-              type="number"
-              name="cvc"
-              placeholder="CVC"
-              value={cvc}
-              onChange={(e) => setCvc(e.target.value)}
-              onFocus={handleInputFocus}
-              className="cvc"
-            />
-          </form>
-        </CardContent>
+        <PaymentTest state={state} setState={setState} />
         <button onClick={finalizePayment}>
           <p>FINALIZAR PAGAMENTO</p>
         </button>
@@ -98,11 +44,11 @@ const PaymentForm = ({ ticket }) => {
   }
 
   function PaymentRender() {
-    if (userTicket.status === 'PAID') return Paid();
+    if (userTicket === 'PAID') return Paid();
     return Reserve();
   }
   async function finalizePayment(e) {
-    e.preventDefault();
+    /*e.preventDefault();
     const cardData = {
       issuer: issuer,
       number: number,
@@ -116,7 +62,7 @@ const PaymentForm = ({ ticket }) => {
       setUserTicket({ ...userTicket, status: 'PAID' });
     } catch (error) {
       alert('Erro ao digitar os dados do cartão');
-    }
+    }*/
   }
 
   return (
@@ -125,7 +71,7 @@ const PaymentForm = ({ ticket }) => {
         <h1>Ingresso e Pagamento</h1>
         <h2>Ingresso Escolhido</h2>
         <TicketContent>
-          <h3>{ticket.TicketType.name}</h3>
+          <h3>Jorge</h3>
           <h4>+ {ticketPriceAjust.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h4>
         </TicketContent>
         <h2>Pagamento</h2>
@@ -133,6 +79,6 @@ const PaymentForm = ({ ticket }) => {
       </PaymentContent>
     </>
   );
-};
+}
 
 export default PaymentForm;
